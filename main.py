142b703418528221430e9b0f401a11aa10d783c0
Ian Wolterstorff <ianwolt@yahoo.com>
2016-11-08 15:21:30 -0500

    Added import statement (they're important)

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ if __name__ == '__main__':
     import sys
     from position import Position
     from randombot import RandomBot
+    from aibot import AIBot
 
     pos = Position()
     # bot = RandomBot()
